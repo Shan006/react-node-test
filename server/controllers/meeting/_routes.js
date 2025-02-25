5a5c4d97c12f019af0c9fc87f2690466f7b214bb
Shan006 <shayanabbasi006@gmail.com>
2025-02-25 19:37:07 +0500

    some small updations 25-02-2025

--- server/controllers/meeting/_routes.js
+++ server/controllers/meeting/_routes.js
@@ -8,19 +8,14 @@ const {
   deleteMany 
 } = require('../../controllers/meeting/meeting');
 
-// GET all meetings with optional filtering
 router.get('/', index);
 
-// GET single meeting by ID
 router.get('/view/:id', view);
 
-// POST create new meeting
 router.post('/add', add);
 
-// DELETE single meeting (soft delete)
 router.delete('/delete/:id', deleteData);
 
-// DELETE multiple meetings (soft delete)
 router.post('/deleteMany', deleteMany);
 
 module.exports = router;
